47a13b0cd7ba16d9579ee268f24db27998596af7
Jess Telmanik <JessTelmanik@Jesss-MacBook-Pro.local>
2017-11-06 17:26:58 -0500

    additional outlet

--- Carpool/EventDetailViewController.swift
+++ Carpool/EventDetailViewController.swift
@@ -15,10 +15,14 @@ class EventDetailViewController: UIViewController {
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var eventDescriptionLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var timeOfDayLabel: UILabel!
+    //@IBOutlet weak var timeOfDayLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dropOffLabel: UILabel!
+    @IBOutlet weak var pickUpLabel: UILabel!
     
     var trip: Trip!
+
+    // update to tableview
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,10 +31,12 @@ class EventDetailViewController: UIViewController {
         formatter.dateFormat = "MMM d, h:mm a"
         let tripDate = formatter.string(from: trip.event.time)
         
+        idLabel.text = "Frank"
         //timeOfDayLabel.text =
         eventDescriptionLabel.text = trip.event.description
         timeLabel.text = tripDate
-        //locationLabel.text = trip.event.location
+        locationLabel.text = "location"
+        //dropOffLabel.text = trip.dropOff
     }
     
 }
