8f794c062e23ba807dfe4df1dd1107bbbfd9a39d
Jess Telmanik <JessTelmanik@Jesss-MacBook-Pro.local>
2017-11-07 10:28:55 -0500

    New CreateVC, change EventVC
    Updated buttons/labels. added CreateVC. changed name of EventVC to TripVC

---
+++ Carpool/CreateTripViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CreateTripViewController.swift
+//  Carpool
+//
+//  Created by Jess Telmanik on 11/7/17.
+//  Copyright © 2017 Immerzion Interactive. All rights reserved.
+//
+
+import UIKit
+
+class CreateTripViewController: UIViewController {
+    
+    
+}
