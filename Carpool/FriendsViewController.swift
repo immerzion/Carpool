cd7329587a7d24a154fb79d50573f978bf4d0692
Jess <jesstelmanik@gmail.com>
2017-11-13 15:21:40 -0500

    FriendsVC

---
+++ Carpool/FriendsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FriendsTripsViewController.swift
+//  Carpool
+//
+//  Created by Jess Telmanik on 11/13/17.
+//  Copyright © 2017 Immerzion Interactive. All rights reserved.
+//
+
+import UIKit
+import CarpoolKit
+import FirebaseCommunity
+
+class FriendsViewController: UITableViewController {
+    
+    @IBOutlet weak var friendName: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+//    func searchForFriends() {
+//
+//        API.search(forUsersWithName: user) { (result) in
+//            <#code#>
+//        }
+//    }
+    
+
+}
