1b215876fea1c165b6e4f83b9adfd8d7242a204f
Immerzion <edimensionality@gmail.com>
2017-11-15 14:50:46 -0500

    kids and junk

--- Carpool/SettingsViewController.swift
+++ Carpool/SettingsViewController.swift
@@ -16,9 +16,44 @@ class SettingsViewController: UITableViewController {
     
     @IBOutlet weak var loginSignOut: UITableViewCell!
     
+    var name = ""
+    var kidsArray: [Child] = []
+    
+    let kidsNames = ["Curly", "Larry", "Moe", "Shemp", "Sue"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        API.fetchCurrentUser { (result) in
+            switch result {
+                
+            case .success(let user):
+                self.name = user.name!
+                
+                if user.children.count > 0 {
+                    self.kidsArray = user.children
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        //temporary add kids in code
+        confirmAddKids()
+    }
+    
+    func loadAllKids() {
+        for kid in kidsNames {
+            API.addChild(name: kid, completion: { (result) in
+                switch result {
+                    
+                case .success(let kid):
+                    print(kid)
+                case .failure(let error):
+                    print(error)
+                }
+            })
+    }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -30,5 +65,23 @@ class SettingsViewController: UITableViewController {
             present(vc, animated: true)
         }
     }
+    
+    func confirmAddKids() {
+        
+        let message = "Do you want to add some random kids to your profile?"
+        // create the alert
+        let alert = UIAlertController(title: "Carpooler", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add action buttons
+        alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: { (alert) in
+            self.loadAllKids()
+        }))
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.cancel, handler: nil))
+        
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
     
 }
