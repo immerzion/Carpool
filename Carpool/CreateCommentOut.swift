78e2e5dd8243df9c3dd785a6f7c285e0529949fe
Immerzion <edimensionality@gmail.com>
2017-11-16 23:28:54 -0500

    LoginVC revamp - includes phone number field & splash scren

--- Carpool/CreateCommentOut.swift
+++ Carpool/CreateCommentOut.swift
@@ -10,6 +10,7 @@
 
 
 //Realtime clock code
+
 //Global Variable
 //var clock: Timer?
 
@@ -21,78 +22,6 @@
 //        nameTextField.text = Date().prettyTime
 //    }
 
-
-
-//
-//func validateText() {
-//    let event = ""
-//    var location =  ""
-//    //var pickUpTime = ""
-//    //var dropOffTime = ""
-//    
-//    //        if eventDescriptionTextField.text == "" {
-//    //            eventDescriptionTextField.textColor = UIColor.red
-//    //        } else {
-//    //            event = eventDescriptionTextField.text!
-//    //        }
-//    
-//    if locationTextField.text == "" {
-//        locationTextField.textColor = UIColor.red
-//    } else {
-//        location = locationTextField.text!
-//    }
-//    
-//    //        if pickUpTimeDisplay.text == "" {
-//    //            pickUpTimeDisplay.textColor = UIColor.red
-//    //        } else {
-//    //            pickUpTime = pickUpTimeDisplay.text!
-//    //        }
-//    //
-//    //        if dropOffTimeDisplay.text == "" {
-//    //            dropOffTimeDisplay.textColor = UIColor.red
-//    //        } else {
-//    //            dropOffTime = dropOffTimeDisplay.text!
-//    //        }
-//    
-//    if location == "" || pickUpTimeDisplay.text == "" {
-//        print("you need to enter more data")
-//        
-//    } else {
-//        pickUpEventDescriptLabel.text = generateEventDescription()
-//        API.createTrip(eventDescription: event, eventTime: pickUpTime, eventLocation: savannah, completion: { (trip) in
-//            print(trip)
-//        })
-//    }
-//}
-//
-//}
-
-
-
-//        if emailTextField.text != nil, passwordTextField.text != nil {
-//
-//            if logSignSegment.selectedSegmentIndex == 0 {
-//                Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
-//                    if let error = error {
-//                        print(#function, error)
-//                    } else {
-//                        UIApplication.shared.isNetworkActivityIndicatorVisible = false //check placement
-//                    }
-//                })
-//            } else {
-//                UIApplication.shared.isNetworkActivityIndicatorVisible = true
-//
-//                    if let error = error {
-//                        //TODO show alert
-//                        print(#function, error)
-//                    } else {
-//                        UIApplication.shared.isNetworkActivityIndicatorVisible = false //check placement
-//                    }
-//                })
-//            }
-//        }
-//    }
-//
 
 
 // Display error message code
